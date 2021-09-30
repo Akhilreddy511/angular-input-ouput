3a73be3bebb0be348385b91cfe4bd7b5cda48da1
Akhilreddy511 <akhilreddynarayana511@gmail.com>
2021-09-30 21:54:19 +0530

    1 file modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,15 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
+import { DataShareService } from './data-share.service';
+import { ComponentOneComponent } from './component-one/component-one.component';
+import { ComponentTwoComponent } from './component-two/component-two.component';
+import { ComponentThreeComponent } from './component-three/component-three.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, ParentComponent, ChildComponent],
+  declarations: [AppComponent, HelloComponent, ParentComponent, ChildComponent, ComponentOneComponent,ComponentTwoComponent,ComponentThreeComponent],
   bootstrap: [AppComponent],
+  providers: [DataShareService]
 })
 export class AppModule {}
